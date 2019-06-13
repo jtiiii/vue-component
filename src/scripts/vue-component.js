88104454e5d3b55cbc5269bc5d0ff2e42924a4ed
陪葬品 <874972403@qq.com>
2019-06-13 14:53:32 +0800

    修复使用resovle => require([...],resolve) 的异步加载带来使用bug问题 版本号+1

--- src/scripts/vue-component.js
+++ src/scripts/vue-component.js
@@ -2,6 +2,8 @@ import Utils from './Utils';
 import Input from './components/input/input';
 import Upload from './components/upload/Upload';
 import Layout from './components/layout/layout';
+import Editor from './components/editor/editor';
+
 
 const TreeNode = resolve => require(['./components/tree/tree-node.vue'], resolve);
 const Panel = resolve => require(['./components/panel/panel.vue'], resolve);
@@ -9,7 +11,6 @@ const Button = resolve => require(['./components/button/button.vue'], resolve);
 const Modal = resolve => require(['./components/modal/modal.vue'], resolve);
 const Search = resolve => require(['./components/search/search.vue'], resolve);
 const Thumbnail = resolve => require(['./components/thumbnail/thumbnail.vue'], resolve);
-const Editor = resolve => require(['./components/editor/editor'], resolve);
 const Dropdown = resolve => require(['./components/dropdown/dropdown.vue'], resolve);
 const Cover = resolve => require(['./components/cover/cover.vue'], resolve);
 
