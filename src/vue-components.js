b3ea7fadc8b05efe3c2f3c83873d1d6491d5d430
陪葬品 <874972403@qq.com>
2019-08-16 02:23:48 +0800

    修复部门加载bug

--- src/vue-components.js
+++ src/vue-components.js
@@ -3,11 +3,9 @@ import Utils from './scripts/Utils';
 const UploadImage = resolve => require(['./components/upload-image.vue'], resolve);
 const ThirdQuill = resolve => require(['./components/third/quill.vue'], resolve);
 const PageBar = resolve => require(['./components/page-bar.vue'], resolve);
-const TreeNode = resolve => require(['./components/tree/tree-node.vue'], resolve);
 const Panel = resolve => require(['./components/panel.vue'], resolve);
 const Button = resolve => require(['./components/button.vue'], resolve);
 const Modal = resolve => require(['./components/modal.vue'], resolve);
-const Search = resolve => require(['./components/search/search.vue'], resolve);
 const Thumbnail = resolve => require(['./components/thumbnail.vue'], resolve);
 const Dropdown = resolve => require(['./components/dropdown.vue'], resolve);
 const Cover = resolve => require(['./components/cover.vue'], resolve);
@@ -17,16 +15,14 @@ const Hint = resolve => require(['./components/hint.vue'],resolve );
 const Text = resolve => require(['./components/input-text.vue'],resolve);
 const File = resolve => require(['./components/input-file.vue'],resolve);
 const Switch = resolve => require(['./components/input-switch.vue'],resolve);
-
-import TreeNodeModel from "./components/tree/TreeNode";
+const Tree = resolve => require(['./components/tree.vue'],resolve);
 
 
 const vueComponents = {
-    TreeNode,
     Button,
     Panel,
     Modal,
-    Search,
+    Tree,
     Input:{ Text, File, Switch},
     Upload:{ Image: UploadImage},
     Thumbnail,
@@ -36,18 +32,7 @@ const vueComponents = {
     Cover,
     Table,
     Selector,
-    Hint};
-const Model = {
-    TreeNode: TreeNodeModel
+    Hint
 };
-// const Store ={
-//     Common: CommonStore
-// };
-// export function register(vue){
-//     let keys = Object.keys(vueComponents);
-//     keys.forEach( key => {
-//         vue.component(vueComponents[key].name,vueComponents[key]);
-//     });
-// }
-export {Model, Utils};
+export {Utils};
 export default vueComponents;
