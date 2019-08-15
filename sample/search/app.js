a8e535bb8cae446a56f277dd967e0c8b825e44cd
陪葬品 <874972403@qq.com>
2019-08-16 02:21:41 +0800

    删掉不需要的文件

--- sample/search/app.js
+++
@@ -1,6 +0,0 @@
-import Search from './sample-search.vue';
-import Vue from '$vue';
-
-new Vue({
-    render: h => h(Search)
-}).$mount('#search');
--- sample/tree/app.js
+++
@@ -1,9 +0,0 @@
-import TreePage from './sample-tree.vue';
-
-import Vue from '$vue';
-
-new Vue({
-    render( h){
-        return h(TreePage)
-    }
-}).$mount('#tree');
